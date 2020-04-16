5c2ffa36c02b381048c9d3a8628054e4c2f1d6a2
Madbus <pitsnthings@gmail.com>
2020-04-16 11:58:15 +0930

    Finished header file. Chnages yet to come though

--- lettucelab.h
+++ lettucelab.h
@@ -17,7 +17,13 @@ class wheels
     void right();
     void forward();
     void backward();
+    void diceRoll();
   private:
+    int _DevisedLeft;
+    int _DevisedForward;
+    int _DevisedBackward;
+    int _DevisedRight;
+    int _DiceRoll;
 };
 
 class lights
@@ -30,6 +36,12 @@ class lights
     void leftTailLight();
     void righTailLight();
   private:
+    int _LeftHeadLight = 4;
+    int _RightHeadLight = 13;
+    int _LeftBlinker = 11;
+    int _RightBlinker = 12;
+    int _LeftTailLight = 8;
+    int _RightTailLight = 7;
 };
 
 #endif
