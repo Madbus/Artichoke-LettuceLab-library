a6f50ccab928c9bb94901c569daa2ecc12aa8e53
Madbus <pitsnthings@gmail.com>
2020-04-16 11:28:21 +0930

    Defined classes

--- lettucelab.h
+++ lettucelab.h
@@ -1,1 +1,31 @@
+#ifndef lettucelab_h
+#define lettucelab_h
 
+#include "Arduino.h"
+
+class lettucelab
+{
+  void begin();
+};
+class wheels
+{
+  public:
+    void disable();
+    void left();
+    void right();
+    void forward();
+    void backward();
+  private:
+};
+
+class lights
+{
+  void leftIndicator();
+  void rightIndicator();
+  void leftHeadLight();
+  void rightHeadLight();
+  void leftTailLight();
+  void righTailLight();
+};
+
+#endif
